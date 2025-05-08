c592028d15f37b9a4fa01fb512610fcec64e7c53
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-08 11:31:32 +0600

    modules :bus:

---
+++ modules.py
@@ -0,0 +1,6 @@
+from some_functions import add_two_nums, make_square, print_fullname as full_name
+
+# print(dir(some_functions))
+print(add_two_nums(1, 2))
+print(make_square(10))
+print(full_name('Hazrat','Ali'))
