9fa54557d227bb7f8c716551e0d06a2600ffa5cb
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-28 09:58:46 +0600

    Range  🚃

--- range.py
+++ range.py
@@ -3,5 +3,5 @@ company = ['NASA','Microsoft','Google','Facebook','Tesla','SpaceX',]
 print (len(company))
 print(range(len(company)))
 for i in range(len(company)):
-    if(company[i]!='BBC' and company[i]!='CNN'):
+    if(company[i]!='NASA' and company[i]!='SpaceX'):
         print ("I like to work in ",company[i].title())
