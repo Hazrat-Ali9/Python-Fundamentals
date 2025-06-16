0baee85d8f65ec7550dcdaa85997f4106587f473
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-16 16:13:19 +0600

    Condition 🥸

---
+++ Condition.py
@@ -0,0 +1,11 @@
+test = input('What is your name:')
+if(test == "Hazrat"):
+    print(test,"Computer Science & Engineering.")
+elif(test =="Sabia"):
+    print(test,"English.")
+elif(test =="Mitu"):
+    print(test,"She is Engineer.")
+elif(test =="Hazrat"):
+    print(test,"lives in Germany.")
+else:
+    print("I don know him, he/she may be a ninja")
